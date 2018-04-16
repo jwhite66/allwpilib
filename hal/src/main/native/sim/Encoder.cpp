152d4d59996c8492323bef634b4d52c81aef2f69
Jeremy White <jwhite@codeweavers.com>
2018-04-15 20:36:55 -0500

    Have the HAL Simulator set the reverse direction on creation.
    This enables a simulator to be aware of the direction.

--- hal/src/main/native/sim/Encoder.cpp
+++ hal/src/main/native/sim/Encoder.cpp
@@ -82,6 +82,7 @@ HAL_EncoderHandle HAL_InitializeEncoder(
   SimEncoderData[index].SetDigitalChannelA(
       getHandleIndex(digitalSourceHandleA));
   SimEncoderData[index].SetInitialized(true);
+  SimEncoderData[index].SetReverseDirection(reverseDirection);
   // TODO: Add encoding type to Sim data
   encoder->index = index;
   encoder->nativeHandle = nativeHandle;
